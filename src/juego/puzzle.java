cfa9fa935a959598d598ca65db0e0aab9f518490
LuisOtonielMaldonadoSoto <luisotoniels@gmail.com>
2020-10-26 14:04:58 -0500

    Juego

--- src/juego/puzzle.java
+++ src/juego/puzzle.java
@@ -1,23 +1,15 @@
 package juego;
 
-import java.util.*;
-import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
-import java.awt.FlowLayout;
-import javax.swing.JTextField;
-import java.awt.GridLayout;
-import java.awt.CardLayout;
-import javax.swing.SpringLayout;
-import javax.swing.SwingConstants;
-import javax.swing.*;
-import java.awt.event.*;
-import java.sql.Ref;
-import java.awt.Color;
 
 
 
